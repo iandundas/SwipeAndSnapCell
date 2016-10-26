51aa1b3035b2d3feac05131fc12f63a9dbcdc8a4
Ian Dundas <contact@iandundas.co.uk>
2016-10-26 13:25:45 +0700

    Added project for Carthage support

---
+++ SwipeAndSnapCell/SwipeAndSnapCell.h
@@ -0,0 +1,19 @@
+//
+//  SwipeAndSnapCell.h
+//  SwipeAndSnapCell
+//
+//  Created by Ian Dundas on 26/10/2016.
+//  Copyright © 2016 IanDundas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwipeAndSnapCell.
+FOUNDATION_EXPORT double SwipeAndSnapCellVersionNumber;
+
+//! Project version string for SwipeAndSnapCell.
+FOUNDATION_EXPORT const unsigned char SwipeAndSnapCellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipeAndSnapCell/PublicHeader.h>
+
+
